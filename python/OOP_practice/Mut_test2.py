fb5c45db5e7f9a53c1fdbaaa21ab0973bf214ceb
David-inkheart <David-Inkheart@github.com>
2022-10-14 10:46:55 +0100

    on properties, setters and getters

---
+++ python/OOP_practice/Mut_test2.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+from mutators import P
+
+p1 = P(42)
+p2 = P(4711)
+a = p1.get_x()
+b = p2.get_x()
+
+print("p1 is: {}\np2 is: {}".format(a, b))
+
+p1.set_x(47)
+p1.set_x(p1.get_x()+p2.get_x())
+a = p1.get_x()
+
+print("new p1 is: {}\np2 is still: {}".format(a, b))
