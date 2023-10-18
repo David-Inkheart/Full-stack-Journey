cabe1e2727b2bc8f5e82cafbb1576b69bd5fed73
David-Inkheart <okoliedave1@gmail.com>
2023-10-18 17:27:43 +0100

    updates

--- FundamentalsOfBE/websocket-push/index.js
+++ FundamentalsOfBE/websocket-push/index.js
@@ -6,7 +6,7 @@ let connections = [];
 const httpserver = http.createServer()
 
  //pass the httpserver object to the WebSocketServer library. this object will override the req/res and will upgrade the protocol to websocket (websocket handshake)
-const websocket = new WebSocketServer({"httpServer": httpserver })
+const websocket = new WebSocketServer({ "httpServer": httpserver })
 //listen on the TCP socket
 httpserver.listen(8080, () => console.log("My server is listening on port 8080"))
 
@@ -14,15 +14,15 @@ httpserver.listen(8080, () => console.log("My server is listening on port 8080")
 //when a legit websocket request comes, listen to it and get the connection ... once you get a connection thats it! 
 websocket.on("request", request => {
 
-    const connection = request.accept(null, request.origin)
+    const connection = request.accept(null, request.origin);
     connection.on("message", message => {
         //someone just sent a message, tell every other connection
         connections.forEach(c => c.send(`User${connection.socket.remotePort} says: ${message.utf8Data}`))
-    })
+    });
     // add new user to the list of connections(other users)
-    connections.push(connection)
+    connections.push(connection);
     //someone just connected, tell everybody
-    connections.forEach(c => c.send(`User${connection.socket.remotePort} just connected.`))
+    connections.forEach(c => c.send(`User${connection.socket.remotePort} just connected.`));
     
     //when someone disconnects, tell everyone
     connection.on("close", () => {
